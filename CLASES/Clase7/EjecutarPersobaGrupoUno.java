5ae6400fd4cb5cb2abad7199135459d840283290
DenzelMora <denzel.mora@campusucc.edu.co>
2025-09-22 21:59:53 +0000

    Tarea

--- CLASES/Clase7/EjecutarPersobaGrupoUno.java
+++ CLASES/Clase7/EjecutarPersobaGrupoUno.java
@@ -7,14 +7,23 @@ public class EjecutarPersobaGrupoUno {
         //Creacion del objeto objPersona1
         Persona objPersona1;
         //Instanciar el objeto (new) --> Instanciar es igual a crear (Darle vida al objeto)
-        objPersona1 = new Persona("Denzel","Mora",17,11072025,62);
+        objPersona1 = new Persona("Denzel","Mora",17,11072025,60.0,1.83);
 
         // Forma2
-        Persona objPersona2 = new Persona("Byron", "Cano", 35, 1001980, 60.0);
+        Persona objPersona2 = new Persona("Byron", "Cano", 35, 1001980, 60.0,1.74);
+
+        Persona objPersona3 = new Persona("Jorge", "Guataqui", 52, 11098765, 70, 1.74);
+
+        Persona objPersona4 = new Persona("Naomi", "Viera", 22, 14374900, 56,1.70);
+
+        Persona objPersona5 = new Persona("Zuleima","Banguera", 45 , 63894655, 80, 1.68 );
 
         // Mostrar por pantalla el objeto creado
         System.out.println(objPersona1.toString());
         System.out.println(objPersona2.toString());
+        System.out.println(objPersona3.toString());
+        System.out.println(objPersona4.toString());
+        System.out.println(objPersona5.toString());
 
         // Mostrar el nombre de la persona
         System.out.println(objPersona1.getNombre());
@@ -24,5 +33,14 @@ public class EjecutarPersobaGrupoUno {
         
         // Mostrar el objPersona2 con la modificacion del nombre
         System.out.println(objPersona2.toString());
+
+        //Saludo
+        System.out.println(objPersona5.saludar());
+
+        // Es mayor de edad 
+        System.out.println(objPersona1.esMayorDeEdad());
+
+        // Calcular imc
+        System.out.println("El imc de la persona es de: " + objPersona4.calcularIMC());
     }
 }
--- CLASES/Clase7/Persona.java
+++ CLASES/Clase7/Persona.java
@@ -8,15 +8,17 @@ public class Persona {
     private int edad;
     private int cedula;
     private double peso;
+    private double altura;
 
     // Constructor de la clase
-    public Persona(String nombre,String apellido,int edad,int cedula,double peso){
+    public Persona(String nombre,String apellido,int edad,int cedula,double peso,double altura){
 
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
         this.cedula = cedula;
         this.peso = peso;
+        this.altura = altura;
     }
 
     // Metodos Getters y Setters
@@ -28,9 +30,61 @@ public class Persona {
         this.nombre =  nombre;
     }
 
+    public int getEdad(){
+        return edad;
+    }
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    public int getCedula(){
+        return cedula;
+    } 
+
+    public void setCedula(int cedula){
+        this.cedula = cedula;
+    }
+
+    public double getPeso(){
+        return peso;
+    }
+
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+    public double getAltura(){
+        return altura;
+    }
+
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+
     // El metodo to string para mostrar los objetos
     public String toString(){
 
-        return "Persona [ Nombre:" + nombre + " Apellido: " + apellido + " Edad: " + edad + " Cedula: " + cedula + " Peso: " + peso + "]";
+        return "Persona [ Nombre:" + nombre + " Apellido: " + apellido + " Edad: " + edad + " Cedula: " + cedula + " Peso: " + peso + " Altura: " + altura + "]";
     }
+
+    //Metodo de saludar
+    public String saludar(){
+        return "Hola " + nombre + ", como te encuentras?";
+    }
+
+    // Metodo de verificar si es mayor de edad
+    public boolean esMayorDeEdad(){
+        if (edad >= 18){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Metodo calcularIMC
+    public double calcularIMC(){
+        return peso / (altura * altura);
+    }
+
 }
