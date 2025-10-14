b2214c8af89b1881cd297459a24f8ca09b5df800
DenzelMora <denzel.mora@campusucc.edu.co>
2025-10-14 12:18:38 -0500

    prueba1

--- CLASES/Principal.java
+++ CLASES/Principal.java
@@ -1,6 +1,6 @@
 ﻿public class Principal {
     public static void main(String[] args){
 
-        CuentaBancaria cuenta1 = new CuentaBancaria("Denzel Mora","123456",0.0);
+        CuentaBancaria cuenta1 = new CuentaBancaria("Denzel Mora","123456errewre",0.0);
     }
 }
